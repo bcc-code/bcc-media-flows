8c90824187a11da4558bcc46972aaeeca35fc856
Fredrik Vedvik <fredrik@vedvik.tech>
2023-08-17 12:00:26 +0200

    remove old transcribe handler

--- cmd/httpin/main.go
+++ cmd/httpin/main.go
@@ -10,70 +10,6 @@ import (
 	"go.temporal.io/sdk/client"
 )
 
-func transcribeHandler(c *gin.Context) {
-	language := c.DefaultPostForm("language", c.DefaultQuery("language", ""))
-	file := c.DefaultPostForm("file", c.DefaultQuery("file", ""))
-	destinationPath := c.DefaultPostForm("destinationPath", c.DefaultQuery("destinationPath", ""))
-	vxID := c.DefaultPostForm("vxID", c.DefaultQuery("vxID", ""))
-
-	wfClient, err := client.Dial(client.Options{
-		HostPort:  os.Getenv("TEMPORAL_HOST_PORT"),
-		Namespace: os.Getenv("TEMPORAL_NAMESPACE"),
-	})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	defer wfClient.Close()
-
-	queue := os.Getenv("QUEUE")
-	if queue == "" {
-		queue = "worker"
-	}
-	workflowOptions := client.StartWorkflowOptions{
-		ID:        "worker-" + uuid.NewString(),
-		TaskQueue: queue,
-	}
-
-	// TODO: Ugly code, just a test
-	if vxID != "" {
-		transcribeInput := workflows.TranscribeVXInput{
-			Language: language,
-			VXID:     vxID,
-		}
-
-		res, err := wfClient.ExecuteWorkflow(c, workflowOptions, workflows.TranscribeVX, transcribeInput)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, res)
-		return
-	}
-
-	transcribeInput := workflows.TranscribeFileInput{
-		Language:        language,
-		File:            file,
-		DestinationPath: destinationPath,
-	}
-
-	res, err := wfClient.ExecuteWorkflow(c, workflowOptions, workflows.TranscribeFile, transcribeInput)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, res)
-}
-
 func getParamFromCtx(ctx *gin.Context, key string) string {
 	return ctx.DefaultPostForm(key, ctx.DefaultQuery(key, ""))
 }
@@ -148,9 +84,6 @@ func triggerHandler(ctx *gin.Context) {
 
 func main() {
 	r := gin.Default()
-
-	r.GET("/transcribe", transcribeHandler)
-	r.POST("/transcribe", transcribeHandler)
 
 	r.POST("/trigger/:job", triggerHandler)
 	r.GET("/trigger/:job", triggerHandler)
