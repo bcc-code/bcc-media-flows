9a5365f27cc5cd17249a25b2d0157f8891d4392f
Fredrik Vedvik <fredrik@vedvik.tech>
2023-08-24 13:37:43 +0200

    bad timer

--- utils/execute_command.go
+++ utils/execute_command.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os/exec"
-	"time"
 )
 
 // ExecuteCmd executes the cmd and returns through outputCallback line-by-line before returning the whole stdout at the end.
@@ -17,28 +16,12 @@ func ExecuteCmd(cmd *exec.Cmd, outputCallback func(string)) (string, error) {
 	}
 
 	var result string
-	var currentLine string
-
-	closeChan := make(chan struct{})
-	defer close(closeChan)
-	timer := time.NewTicker(time.Second * 5)
-	go func() {
-		for {
-			select {
-			case <-timer.C:
-				fmt.Printf("currentline %s\n", currentLine)
-			case <-closeChan:
-				return
-			}
-		}
-	}()
 
 	scanner := bufio.NewScanner(stdout)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		line := scanner.Text()
 		result += line + "\n"
-		currentLine = fmt.Sprintf("%s, currentline %s\n", time.Now().Format(time.RFC3339), line)
 		if outputCallback != nil {
 			outputCallback(line)
 		}
