5351e3f367106899a82bf230f095ce4459451689
Fredrik Vedvik <fredrik@vedvik.tech>
2023-11-29 14:53:54 +0100

    chore: move some flows out of main

--- cmd/trigger_ui/main.go
+++ cmd/trigger_ui/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"path/filepath"
-	"time"
 
 	"github.com/bcc-code/bccm-flows/environment"
 	"github.com/google/uuid"
@@ -22,8 +19,6 @@ import (
 	"github.com/bcc-code/bccm-flows/services/vidispine/vscommon"
 	"github.com/bcc-code/bccm-flows/workflows/export"
 	"github.com/gin-gonic/gin"
-	"go.temporal.io/api/history/v1"
-	"go.temporal.io/api/workflowservice/v1"
 	"go.temporal.io/sdk/client"
 )
 
@@ -235,89 +230,6 @@ func (s *TriggerServer) triggerHandlerPOST(c *gin.Context) {
 	})
 }
 
-type WorkflowListParams struct {
-	WorkflowList     []WorkflowDetails
-	WorkflowStatuses map[string]string
-}
-
-type WorkflowDetails struct {
-	VxID       string
-	Name       string
-	Status     string
-	WorkflowID string
-	Start      string
-}
-
-func (s *TriggerServer) listGET(c *gin.Context) {
-	var workflowList []WorkflowDetails
-
-	workflows, err := s.wfClient.ListWorkflow(c, &workflowservice.ListWorkflowExecutionsRequest{
-		Query: "WorkflowType = 'VXExport'",
-	})
-	if err != nil {
-		renderErrorPage(c, http.StatusInternalServerError, err)
-		return
-	}
-
-	for i := 0; i < len(workflows.Executions); i++ {
-		res, err := s.wfClient.WorkflowService().GetWorkflowExecutionHistory(c, &workflowservice.GetWorkflowExecutionHistoryRequest{
-			Execution: workflows.Executions[i].GetExecution(),
-			Namespace: os.Getenv("TEMPORAL_NAMESPACE"),
-		})
-
-		if err != nil {
-			renderErrorPage(c, http.StatusInternalServerError, err)
-			return
-		}
-		attributes, ok := res.History.Events[0].Attributes.(*history.HistoryEvent_WorkflowExecutionStartedEventAttributes)
-		if !ok {
-			renderErrorPage(c, 500, errors.New("unexpected attribute type on first workflow event. Was not HistoryEvent_WorkflowExecutionStartedEventAttributes"))
-			break
-		}
-
-		data := export.VXExportParams{}
-		err = json.Unmarshal(attributes.WorkflowExecutionStartedEventAttributes.Input.Payloads[0].Data, &data)
-
-		if err != nil {
-			renderErrorPage(c, http.StatusInternalServerError, err)
-			return
-		}
-
-		meta, err := s.vidispine.GetMetadata(data.VXID)
-		if err != nil {
-			renderErrorPage(c, http.StatusInternalServerError, err)
-			return
-		}
-		name := meta.Get(vscommon.FieldTitle, "")
-
-		loc, _ := time.LoadLocation("Europe/Oslo")
-		startTime := workflows.Executions[i].StartTime.In(loc).Format("Mon, 02 Jan 2006 15:04:05 MST")
-
-		workflowList = append(workflowList, WorkflowDetails{
-			VxID:       data.VXID,
-			Name:       name,
-			Status:     workflows.Executions[i].GetStatus().String(),
-			WorkflowID: workflows.Executions[i].Execution.WorkflowId,
-			Start:      startTime,
-		})
-
-	}
-
-	workflowStatuses := map[string]string{
-		"Running":    "blue",
-		"Timed out":  "yellow",
-		"Completed":  "green",
-		"Failed":     "red",
-		"Canceled":   "yellow",
-		"Terminated": "red",
-	}
-
-	c.HTML(http.StatusOK, "list.gohtml", WorkflowListParams{
-		WorkflowList:     workflowList,
-		WorkflowStatuses: workflowStatuses,
-	})
-}
-
 func main() {
 	router := gin.Default()
 
@@ -383,5 +295,8 @@ func main() {
 	}
 
 	fmt.Printf("Started on port %s", port)
-	router.Run(fmt.Sprintf(":%s", port))
+	err = router.Run(fmt.Sprintf(":%s", port))
+	if err != nil {
+		panic(err)
+	}
 }
