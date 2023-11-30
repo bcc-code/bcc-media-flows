e4d7b0f8bd5a09018d82ad4305de45c32b2d2d76
Fredrik Vedvik <fredrik@vedvik.tech>
2023-11-30 15:56:24 +0100

    use correct task queue

--- utils/workflows/notify.go
+++ utils/workflows/notify.go
@@ -2,14 +2,12 @@ package wfutils
 
 import (
 	"github.com/bcc-code/bccm-flows/activities"
-	"github.com/bcc-code/bccm-flows/environment"
 	"github.com/bcc-code/bccm-flows/services/notifications"
 	"go.temporal.io/sdk/workflow"
 )
 
 func Notify(ctx workflow.Context, targets []notifications.Target, title, message string) error {
-	ctx = workflow.WithTaskQueue(ctx, environment.QueueWorker)
-	return workflow.ExecuteActivity(ctx, activities.NotifyTargets, activities.NotifyTargetsInput{
+	return ExecuteWithQueue(ctx, activities.NotifyTargets, activities.NotifyTargetsInput{
 		Targets: targets,
 		Message: notifications.SimpleNotification{
 			Title:   title,
