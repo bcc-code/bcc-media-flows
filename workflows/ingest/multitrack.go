fb574fe63439f1956699230892477a6946e7d603
Fredrik Vedvik <fredrik@vedvik.tech>
2023-12-08 13:46:29 +0100

    feat(multitrack): remove unused code

--- workflows/ingest/multitrack.go
+++ workflows/ingest/multitrack.go
@@ -2,13 +2,10 @@ package ingestworkflows
 
 import (
 	"sort"
-	"strconv"
-	"strings"
 
 	"github.com/bcc-code/bccm-flows/activities"
 	"github.com/bcc-code/bccm-flows/paths"
 	wfutils "github.com/bcc-code/bccm-flows/utils/workflows"
-	"github.com/samber/lo"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -101,52 +98,4 @@ func Multitrack(ctx workflow.Context, params MasterParams) (*MasterResult, error
 	}
 
 	return nil, nil
-}
-
-func filesToChannelSources(files []paths.Path) (channelSources, error) {
-	var sources channelSources
-	for _, f := range files {
-		r, err := getChannelSourcesFromFile(f)
-		if err != nil {
-			return nil, err
-		}
-		sources = append(sources, r...)
-	}
-
-	sort.Sort(sources)
-	return sources, nil
-}
-
-func getChannelSourcesFromFile(file paths.Path) ([]channelSource, error) {
-	base := file.Base()
-	name := base[:len(base)-len(file.Ext())]
-	parts := strings.Split(name, "_")
-	last, _ := lo.Last(parts)
-	if last == "" {
-		return nil, nil
-	}
-
-	channel1, err := strconv.Atoi(string(last[0]))
-	if err != nil {
-		return nil, err
-	}
-	sources := []channelSource{
-		{
-			Order:   channel1,
-			Channel: 0,
-			Path:    file,
-		},
-	}
-	if len(last) == 1 {
-		return sources, nil
-	}
-	channel2, err := strconv.Atoi(string(last[1]))
-	if err != nil {
-		return nil, err
-	}
-	return append(sources, channelSource{
-		Order:   channel2,
-		Channel: 1,
-		Path:    file,
-	}), nil
 }
