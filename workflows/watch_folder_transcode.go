6a6e7a5ba5abcedb59a2bf8550cce99336725f83
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-01-09 13:45:59 +0100

    Fix the fixed fix

--- workflows/watch_folder_transcode.go
+++ workflows/watch_folder_transcode.go
@@ -123,8 +123,8 @@ func WatchFolderTranscode(ctx workflow.Context, params WatchFolderTranscodeInput
 		ctx = workflow.WithTaskQueue(ctx, environment.GetWorkerQueue())
 		err = workflow.ExecuteChildWorkflow(ctx, TranscribeFile, TranscribeFileInput{
 			Language:        "no",
-			File:            path.Linux(),
-			DestinationPath: outFolder.Linux(),
+			File:            path,
+			DestinationPath: outFolder,
 		}).Get(ctx, nil)
 	default:
 		err = fmt.Errorf("codec not supported: %s", params.FolderName)
