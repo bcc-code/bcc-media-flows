1959d139d572398bfbd4fc63263eee2653eb3c34
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-03-24 11:09:54 +0100

    Hitfix click

--- workflows/vb_export/vb_export.go
+++ workflows/vb_export/vb_export.go
@@ -126,7 +126,7 @@ func VBExport(ctx workflow.Context, params VBExportParams) ([]wfutils.ResultOrEr
 		return *dest != DestinationHippo
 	})
 
-	if len(destinationsWithAudioOutput) > 0 && analyzeResult.HasAudio {
+	if len(destinationsWithAudioOutput) > 0 && analyzeResult.HasAudio && !strings.Contains(videoFilePath.Base(), "_KLICK") {
 		normalizeAudioResult, err := wfutils.Execute(ctx, activities.Audio.NormalizeAudioActivity, activities.NormalizeAudioParams{
 			FilePath:              videoFilePath,
 			TargetLUFS:            -23,
