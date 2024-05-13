8d25f1645998ce92d695b40e0b39c9b35a154dab
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-05-13 17:11:46 +0200

    Add Centemo API calls required by the BCCM Tools
    * Get transcription JSON
    * Get Preview

--- services/cantemo/client.go
+++ services/cantemo/client.go
@@ -31,3 +31,64 @@ func (c *Client) AddRelation(parent, child string) error {
 
 	return err
 }
+
+func (c *Client) GetFormats(itemID string) ([]Format, error) {
+
+	req := c.restyClient.R().SetResult(&GetFormatsResponse{})
+	res, err := req.Get("/API/v2/items/" + itemID + "/formats/")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Result().(*GetFormatsResponse).Formats, err
+}
+
+func (c *Client) GetMetadata(itemID string) (*ItemMetadata, error) {
+	res, err := c.restyClient.R().SetResult(&ItemMetadata{}).
+		Get("/API/v2/items/" + itemID + "/")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Result().(*ItemMetadata), nil
+}
+
+func (c *Client) GetPreviewUrl(itemID string) (string, error) {
+	meta, err := c.GetMetadata(itemID)
+	if err != nil {
+		return "", err
+	}
+
+	for _, s := range meta.Previews.Shapes {
+		return c.baseURL + s.URI, nil
+	}
+
+	return "", nil
+}
+
+func (c *Client) GetTranscriptionJSON(itemID string) (*Transcription, error) {
+	formats, err := c.GetFormats(itemID)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, format := range formats {
+		if format.Name != "transcription_json" {
+			continue
+		}
+
+		res, err := c.restyClient.R().
+			SetResult(&Transcription{}).
+			Get("/vs/item/download/VX-486350/?shape=VX-978080")
+
+		if err != nil {
+			return nil, err
+		}
+
+		return res.Result().(*Transcription), nil
+	}
+
+	return &Transcription{}, nil
+}
--- services/cantemo/client_test.go
+++ services/cantemo/client_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-// TestAddRelation tests is a bare bones check,
+// TestAddRelation tests is a bare-bones check,
 // it does not check if the relation was actually added.
 func TestAddRelation(t *testing.T) {
 	cantemoBaseURL := os.Getenv("CANTEMO_URL")
@@ -23,4 +23,40 @@ func TestAddRelation(t *testing.T) {
 	client := NewClient(cantemoBaseURL, cantemoToken)
 	err := client.AddRelation("VX-parent", "VX-child")
 	assert.NoError(t, err)
+}
+
+func TestClient_GetTranscriptionJSON(t *testing.T) {
+	baseURL := os.Getenv("CANTEMO_URL")
+	token := os.Getenv("CANTEMO_TOKEN")
+
+	if baseURL == "" {
+		t.Skip("CANTEMO_URL not set")
+	}
+
+	if token == "" {
+		t.Skip("CANTEMO_TOKEN not set")
+	}
+
+	client := NewClient(baseURL, token)
+	tr, err := client.GetTranscriptionJSON("VX-486350")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tr)
+}
+
+func TestClient_GetPreview(t *testing.T) {
+	baseURL := os.Getenv("CANTEMO_URL")
+	token := os.Getenv("CANTEMO_TOKEN")
+
+	if baseURL == "" {
+		t.Skip("CANTEMO_URL not set")
+	}
+
+	if token == "" {
+		t.Skip("CANTEMO_TOKEN not set")
+	}
+
+	client := NewClient(baseURL, token)
+	meta, err := client.GetPreviewUrl("VX-486350")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, meta)
 }
