6ce7679190c9c4c868c6768264e51a1b93fe71fd
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-08-14 09:21:29 +0200

    Merge two nearly identical tests

--- services/vidispine/chapter_test.go
+++ services/vidispine/chapter_test.go
@@ -55,83 +55,65 @@ func TestMergeTerseTimecodes(t *testing.T) {
 
 // This is an edge case where the annotations overlap by a few frames
 func Test_GetChapterMetaForClips_Overlapping(t *testing.T) {
-	clips := []*Clip{
-		{
-			VideoFile:   "/dummy/file.mp4",
-			InSeconds:   1420.1199999999953,
-			OutSeconds:  2767.439999999988,
-			SequenceIn:  0,
-			SequenceOut: 0,
-			AudioFiles: map[string]*AudioFile{
-				"nor": {
-					VXID:    "VX-489605",
-					Streams: []int{2},
-					File:    "/dummy/file.wav",
-				},
-			},
-			SubtitleFiles:      map[string]string{},
-			JSONTranscriptFile: "",
-			VXID:               "VX-489598",
-		},
-	}
-
 	if os.Getenv("VIDISPINE_BASE_URL") == "" {
 		t.Skip("VIDISPINE_BASE_URL is not set")
 	}
 
-	client := vsapi.NewClient(
-		os.Getenv("VIDISPINE_BASE_URL"),
-		os.Getenv("VIDISPINE_USERNAME"),
-		os.Getenv("VIDISPINE_PASSWORD"),
-	)
-	out, err := GetChapterMetaForClips(client, clips)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, out)
-	assert.Len(t, out, 1)
-
-	for i, chapter := range out {
-		assert.GreaterOrEqual(t, len(chapter.Meta.Terse["title"]), 1, "Chapter in loop iteration %d has no title", i)
-	}
-}
-
-// This is an edge case where the annotations overlap by a few frames
-// This test is identical to Test_GetChapterMetaForClips_Overlapping but with a different subclip that was exibithing the same behaviour
-func Test_GetChapterMetaForClips_Overlapping2(t *testing.T) {
-	clips := []*Clip{
+	testData := [][]*Clip{
 		{
-			VideoFile:   "/dummy/file.mp4",
-			InSeconds:   3750.9199999999983,
-			OutSeconds:  3906.87999999999,
-			SequenceIn:  0,
-			SequenceOut: 0,
-			AudioFiles: map[string]*AudioFile{
-				"nor": {
-					VXID:    "VX-489605",
-					Streams: []int{2},
-					File:    "/dummy/file.wav",
+			{
+				VideoFile:   "/dummy/file.mp4",
+				InSeconds:   1420.1199999999953,
+				OutSeconds:  2767.439999999988,
+				SequenceIn:  0,
+				SequenceOut: 0,
+				AudioFiles: map[string]*AudioFile{
+					"nor": {
+						VXID:    "VX-489605",
+						Streams: []int{2},
+						File:    "/dummy/file.wav",
+					},
 				},
+				SubtitleFiles:      map[string]string{},
+				JSONTranscriptFile: "",
+				VXID:               "VX-489598",
 			},
-			SubtitleFiles:      map[string]string{},
-			JSONTranscriptFile: "",
-			VXID:               "VX-489598",
+		},
+		{
+			{
+				VideoFile:   "/dummy/file.mp4",
+				InSeconds:   3750.9199999999983,
+				OutSeconds:  3906.87999999999,
+				SequenceIn:  0,
+				SequenceOut: 0,
+				AudioFiles: map[string]*AudioFile{
+					"nor": {
+						VXID:    "VX-489605",
+						Streams: []int{2},
+						File:    "/dummy/file.wav",
+					},
+				},
+				SubtitleFiles:      map[string]string{},
+				JSONTranscriptFile: "",
+				VXID:               "VX-489598",
+			},
 		},
 	}
 
-	if os.Getenv("VIDISPINE_BASE_URL") == "" {
-		t.Skip("VIDISPINE_BASE_URL is not set")
-	}
+	for _, clips := range testData {
 
-	client := vsapi.NewClient(
-		os.Getenv("VIDISPINE_BASE_URL"),
-		os.Getenv("VIDISPINE_USERNAME"),
-		os.Getenv("VIDISPINE_PASSWORD"),
-	)
-	out, err := GetChapterMetaForClips(client, clips)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, out)
-	assert.Len(t, out, 1)
+		client := vsapi.NewClient(
+			os.Getenv("VIDISPINE_BASE_URL"),
+			os.Getenv("VIDISPINE_USERNAME"),
+			os.Getenv("VIDISPINE_PASSWORD"),
+		)
+		out, err := GetChapterMetaForClips(client, clips)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, out)
+		assert.Len(t, out, 1)
 
-	for i, chapter := range out {
-		assert.GreaterOrEqual(t, len(chapter.Meta.Terse["title"]), 1, "Chapter in loop iteration %d has no title", i)
+		for i, chapter := range out {
+			assert.GreaterOrEqual(t, len(chapter.Meta.Terse["title"]), 1, "Chapter in loop iteration %d has no title", i)
+		}
 	}
 }
