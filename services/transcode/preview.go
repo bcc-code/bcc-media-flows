34e397853a65d57904ddf982d3b462dbef6a4e29
Matjaz Debelak <matjaz.debelak@bcc.no>
2024-11-08 12:41:36 +0100

    Fix audio previews for MU1/2

--- services/transcode/preview.go
+++ services/transcode/preview.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	bccmflows "github.com/bcc-code/bcc-media-flows"
@@ -29,19 +30,14 @@ type AudioPreviewResult struct {
 
 var previewWatermarkPath = environment.GetIsilonPrefix() + "/system/graphics/LOGO_BTV_Preview_960-540.mov"
 
-func AudioPreview(input PreviewInput, progressCallback ffmpeg.ProgressCallback) (*AudioPreviewResult, error) {
-	out := &AudioPreviewResult{}
+type audioPreviewData struct {
+	FFMPEGParams []string
+	LanguageMap  map[string]string
+}
 
-	info, err := ffmpeg.ProbeFile(input.FilePath)
-	if err != nil {
-		return nil, err
-	}
-
-	isMU1 := strings.Contains(input.FilePath, "_MU1")
-	isMU2 := strings.Contains(input.FilePath, "_MU2")
-
+func prepareAudioPreview(isMU1, isMU2 bool, fileInfo *ffmpeg.FFProbeResult, inputFile, outputDir string) (*audioPreviewData, error) {
 	audioStreams := []ffmpeg.FFProbeStream{}
-	for _, stream := range info.Streams {
+	for _, stream := range fileInfo.Streams {
 		if stream.CodecType == "audio" {
 			audioStreams = append(audioStreams, stream)
 		}
@@ -54,58 +50,98 @@ func AudioPreview(input PreviewInput, progressCallback ffmpeg.ProgressCallback) 
 	if len(audioStreams) == 16 {
 		if isMU1 {
 			for i, l := range bccmflows.LanguagesByMU1 {
-				fileName := filepath.Join(input.OutputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
+				if l.MU1ChannelStart != i {
+					continue // skip duplicated languages
+				}
+
+				fileName := filepath.Join(outputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
 
 				if l.MU1ChannelCount == 1 {
-					audioMap = append(audioMap, "-map", fmt.Sprintf("[0:%d]", l.MU1ChannelStart), fileName)
+					filterParts = append(filterParts, fmt.Sprintf("[0:%d]acopy[a%d]", l.MU1ChannelStart, i))
 				} else {
 					filterParts = append(filterParts, fmt.Sprintf("[0:%d][0:%d]amerge=inputs=2[a%d]", l.MU1ChannelStart, l.MU1ChannelStart+1, i))
-					audioMap = append(audioMap, "-map", fmt.Sprintf("[a%d]", i), fileName)
 				}
+
+				audioMap = append(audioMap, "-map", fmt.Sprintf("[a%d]", i), fileName)
 
 				fileMap[l.ISO6391] = fileName
 			}
 		} else if isMU2 {
 			for i, l := range bccmflows.LanguagesByMU2 {
-				fileName := filepath.Join(input.OutputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
-
-				if l.MU2ChannelCount == 1 {
-					audioMap = append(audioMap, "-map", fmt.Sprintf("[0:%d]", l.MU2ChannelStart), fileName)
-				} else {
-					filterParts = append(filterParts, fmt.Sprintf("[0:%d][0:%d]amerge=inputs=2[a%d]", l.MU2ChannelStart, l.MU2ChannelStart+1, i))
-					audioMap = append(audioMap, "-map", fmt.Sprintf("[a%d]", i), fileName)
+				if l.MU2ChannelStart != i {
+					continue // skip duplicated languages
 				}
 
+				fileName := filepath.Join(outputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
+
+				if l.MU2ChannelCount == 1 {
+					filterParts = append(filterParts, fmt.Sprintf("[0:%d]acopy[a%d]", l.MU2ChannelStart, i))
+				} else {
+					filterParts = append(filterParts, fmt.Sprintf("[0:%d][0:%d]amerge=inputs=2[a%d]", l.MU2ChannelStart, l.MU2ChannelStart+1, i))
+				}
+
+				audioMap = append(audioMap, "-map", fmt.Sprintf("[a%d]", i), fileName)
 				fileMap[l.ISO6391] = fileName
 			}
 		} else {
-			return nil, fmt.Errorf("Unknow format of audio channels. Not generating preview")
+			return nil, fmt.Errorf("unknown format of audio channels. Not generating preview")
 		}
 
 	} else if len(audioStreams) == 1 && audioStreams[0].Channels == 64 {
 		for i, l := range bccmflows.LanguageBySoftron {
-			fileName := filepath.Join(input.OutputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
+			fileName := filepath.Join(outputDir, fmt.Sprintf("%d.%s.aac", i, l.ISO6391))
 			filterParts = append(filterParts, fmt.Sprintf("[0:%d]pan=stereo|c0=c%d|c1=c%d[a%d]", audioStreams[0].Index, l.SoftronStartCh, l.SoftronStartCh+1, i))
 			audioMap = append(audioMap, "-map", fmt.Sprintf("[a%d]", i), fileName)
 			fileMap[l.ISO6391] = fileName
 		}
 	} else {
-		return out, nil
+		return nil, nil
 	}
 
-	out.AudioTracks = fileMap
+	// This is here to stabilize the string for unit tests
+	sort.Strings(filterParts)
 
 	args := []string{
-		"-i", input.FilePath,
+		"-i", inputFile,
 		"-c:a", "aac", "-b:a", "64k", "-ar", "44100", "-ac", "2", "-profile:a", "aac_low",
 		"-filter_complex", strings.Join(filterParts, ";"),
+		"-y",
 	}
+
 	args = append(args, audioMap...)
 
-	_, err = ffmpeg.Do(args, ffmpeg.ProbeResultToInfo(info), progressCallback)
+	return &audioPreviewData{
+		LanguageMap:  fileMap,
+		FFMPEGParams: args,
+	}, nil
+}
+
+func AudioPreview(input PreviewInput, progressCallback ffmpeg.ProgressCallback) (*AudioPreviewResult, error) {
+	out := &AudioPreviewResult{}
+
+	isMU1 := strings.Contains(input.FilePath, "_MU1")
+	isMU2 := strings.Contains(input.FilePath, "_MU2")
+
+	info, err := ffmpeg.ProbeFile(input.FilePath)
 	if err != nil {
 		return nil, err
 	}
+
+	previewData, err := prepareAudioPreview(isMU1, isMU2, info, input.FilePath, input.OutputDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if previewData == nil {
+		return out, nil
+	}
+
+	_, err = ffmpeg.Do(previewData.FFMPEGParams, ffmpeg.ProbeResultToInfo(info), progressCallback)
+	if err != nil {
+		return nil, err
+	}
+
+	out.AudioTracks = previewData.LanguageMap
 
 	return out, nil
 }
